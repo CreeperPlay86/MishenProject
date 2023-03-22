64a1c6e1ed91e221c8c8f2d41a038263667827d2
CreeperPlay86 <102041933+CreeperPlay86@users.noreply.github.com>
2023-03-22 15:00:16 +0500

    TIMER

---
+++ Assets/Timer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart = 60;
+    public TMP_Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        timerText.text = Mathf.Round(timeStart).ToString();
+    }
+}
