ef1f333681ecd6eaba56796665e6f80c0503e1e8
CreeperPlay86 <102041933+CreeperPlay86@users.noreply.github.com>
2023-03-23 11:11:02 +0500

    ojihfy

--- Assets/Scripts/Spawner_2.cs
+++ Assets/Scripts/Spawner_2.cs
@@ -15,8 +15,8 @@ public class Spawner_2 : MonoBehaviour
     private IEnumerator CreateObject()
     {
 
-        yield return new WaitForSeconds(1);
-        Instantiate(prop, new Vector3(Random.Range(-8.836f, -24.02f), 4.525f, Random.Range(-19.207f, -5.66f)), Quaternion.Euler(new Vector3(-90,0,0)));
+        yield return new WaitForSeconds(5);
+        Instantiate(prop, new Vector3(Random.Range(-8.836f, -24.02f), 4.525f, Random.Range(-10.04f, -5.66f)), Quaternion.Euler(new Vector3(-90,0,0)));
         StartCoroutine(CreateObject());
     }
 }
